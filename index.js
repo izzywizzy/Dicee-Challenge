ee2c62f41cf4557fcaf9ad84248f899b83863b7d
izzychuks <ceziakonwa@gmail.com>
2022-10-03 03:29:43 +0100

    finished the dice challenge

---
+++ index.js
@@ -0,0 +1,30 @@
+// alert("hello World")
+
+var randomNumber1 = Math.floor(Math.random() *6) + 1; // create a random number between 1 - 6
+
+var randomNumber2 = Math.floor(Math.random() *6) + 1; // create a random number between 1 - 6
+
+var Winner ; //create a variable for the winner
+
+var randomDiceImageSelected = "./images/"+"dice" + randomNumber1 +".png" // create avariable to hold a  random dice image 1
+
+var randomDiceImage2Selected = "./images/"+"dice" + randomNumber2 +".png" // create avariable to hold a  random dice image 2
+
+
+ document.querySelectorAll('img')[0].setAttribute("src",randomDiceImageSelected);
+
+ document.querySelectorAll('img')[1].setAttribute("src",randomDiceImage2Selected);
+
+
+
+if(randomNumber1 > randomNumber2)
+        {
+        Winner = "The Winner is player 1"
+        } 
+else if (randomNumber1 < randomNumber2)
+        {
+            Winner = "The Winner is player 2"
+        }
+    else { Winner = "It's a draw"}
+
+document.querySelector('h2').innerText = Winner ; 
